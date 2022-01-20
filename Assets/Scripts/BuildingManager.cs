ad8084e16d1992efe833525e406272534e4a2a68
epanlauw <epanlauw6@gmail.com>
2022-01-20 14:19:50 +0700

    Enemy Wave Manager

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -58,12 +58,6 @@ public class BuildingManager : MonoBehaviour
                 }
             }
         }
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            Vector3 enemySpawnPosition = UtilsClass.GetMouseWorldPosition() + UtilsClass.GetRandomDir() * 5f;
-            Enemy.Create(enemySpawnPosition);
-        }
     }
 
    
